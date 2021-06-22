9877abdcda58372c2816a9605bff9d08bb01a06d
JunzhengLiu-new <71883592+JunzhengLiu-new@users.noreply.github.com>
2021-06-22 12:09:24 +0100

    Create quickcreate_database_script.py

---
+++ dublin_bus_app/data_collection/quickcreate_database_script.py
@@ -0,0 +1,31 @@
+from pymysql import connect
+class create_database:
+    def __init__(self):
+        pass
+    def create_table(self):
+        conn = connect(host="localhost", port=3306, user="root", password="mysql", database="project",
+                            charset="utf8")
+        cs = conn.cursor()
+        try:
+            try:
+                cs.execute("drop table ")
+            except Exception as e:
+                pass
+            value_1 = "create table aaa (timestamp int,id VARCHAR(45),trip_id VARCHAR(45),start_time VARCHAR(45),start_date VARCHAR(45),route_id VARCHAR(45),stop_sequence INT,arrival_delay INT,departure_delay INT,stop_id VARCHAR(45))"
+            cs.execute(value_1)
+            try:
+                cs.execute("drop table bbb")
+            except Exception as e:
+                pass
+            value_2 = "create table bbb (dt INT, temp INT,humidity INT,speed INT, description VARCHAR(45))"
+            cs.execute(value_2)
+            conn.commit()
+        except Exception as e:
+            print("error")
+        cs.close()
+        conn.close()
+    def run(self):
+        self.create_table()
+if __name__ == "__main__":
+    Bike = create_database()
+    Bike.run()
