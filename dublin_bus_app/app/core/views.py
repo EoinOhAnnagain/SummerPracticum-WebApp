526a89feab2ae1be118d272a072582d98e4a017c
EJEgan <eugene.egan1@ucdconnect.ie>
2021-06-30 13:19:41 +0100

    added function for stop data access from local JSON file

--- dublin_bus_app/app/core/views.py
+++ dublin_bus_app/app/core/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.http import JsonResponse
+from django.contrib.staticfiles import finders
+
+import json
+
+def Stops(request):
+   # stopsPath = finders.find("JSON/stops.json")
+   # stopsString = json.loads(stopsPath)
+   # print(stopsString)
+   json_data = open(finders.find('JSON/stops.json'))
+   data1 = json.load(json_data)  # deserialises it
+   data2 = json.dumps(data1)  # json formatted string
+   json_data.close()
+   return JsonResponse(data2, safe=False)
+
+
 def HelloWorldView(request):
     response = HttpResponse("Hello World!")
     return response
