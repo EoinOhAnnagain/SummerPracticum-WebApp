8f343d5189503187663580bb5b0bb5428f800c8c
EJEgan <eugene.egan1@ucdconnect.ie>
2021-06-30 13:19:16 +0100

    added url for stop data access

--- dublin_bus_app/app/core/urls.py
+++ dublin_bus_app/app/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path # new
-from .views import HelloWorldView, MainPageView, MapView
+from .views import HelloWorldView, MainPageView, MapView, Stops
 urlpatterns = [
     path('helloworld', HelloWorldView, name='HelloWorld'),
     path('', MainPageView, name='MainPageView'),
     path('map', MapView, name='MapView'),
+    path('stops', Stops, name='Stops'),
 ]
