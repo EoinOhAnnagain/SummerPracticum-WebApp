3c57f15ac0fff736685b96b7b0e15ef8d9d216f9
Eoinoha <eoin.ohannagain@ucdconnect.ie>
2021-07-09 14:24:16 +0100

    Implement sign in system

--- iOS/DBA/DBA/View/LoginViewController.swift
+++ iOS/DBA/DBA/View/LoginViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
     
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
 
     override func viewDidLoad() {
@@ -25,6 +28,23 @@ class LoginViewController: UIViewController {
     @IBAction func signUpPressed(_ sender: UIButton) {
         
         performSegue(withIdentifier: "LoginToSignUp", sender: self)
+        
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    print("Successful Sign In")
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        } else {
+            print("no email or password")
+        }
         
     }
     
