566c9975e5a4ebeefb656cd4c4048ad41a740f13
Eoinoha <eoin.ohannagain@ucdconnect.ie>
2021-07-14 14:16:32 +0100

    Split chat based on bus routes. Implement default message in case of chat unused.

--- iOS/DBA/DBA/Controller/ChatViewController.swift
+++ iOS/DBA/DBA/Controller/ChatViewController.swift
@@ -14,11 +14,17 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextField: UITextField!
     @IBOutlet weak var infoLabel: UILabel!
-     
+    
     let db = Firestore.firestore()
     
     var messages: [Message] = []
     
+    var chosenChat: String = "messages"
+    
+    
+    @IBOutlet weak var chatPicker: UIPickerView!
+    
+    var busStops = ["H1", "H2", "H3", "6", "1", "4", "7", "7a", "7b", "7n", "9", "11", "13", "14", "15", "15a", "15b", "15d", "16", "25", "25a", "25b", "25d", "25n", "25x", "26", "27", "27a", "27b", "27x", "29a", "29n", "31/a", "31b", "31d", "31n", "32", "32x", "33", "33s", "33n", "33x", "37", "38a", "38b", "39", "39a", "39x", "40", "40b", "40d", "40e", "41", "41b", "41c", "41x", "42", "41d", "42d", "42n", "43", "44", "44b", "46a", "46e", "46n", "47", "49", "49n", "51d", "51x", "53", "53a", "54a", "56a", "61", "65", "65b", "66", "66a", "66b", "66e", "66n", "66x", "67", "67n", "67x", "68/a", "68x", "69", "69n", "69x", "70", "70d", "70n", "77a", "77n", "77x", "79/a", "83", "84/a", "84n", "84x", "88n", "90", "116", "118", "120", "122", "123", "130", "140", "142", "145", "150", "151", "155", "747", "757"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +33,10 @@ class ChatViewController: UIViewController {
         
         tableView.dataSource = self
         tableView.delegate = self
-    
+        
+        chatPicker.dataSource = self
+        chatPicker.delegate = self
+        
         
         tableView.register(UINib(nibName: K.chat.chatNib, bundle: nil), forCellReuseIdentifier: K.chat.chatCellID)
         // Do any additional setup after loading the view.
@@ -35,41 +44,57 @@ class ChatViewController: UIViewController {
     
     
     func loadMessages() {
-        db.collection(K.chat.FStore.collectionName)
+        db.collection(chosenChat)
             .order(by: K.chat.FStore.dateField)
             .addSnapshotListener { (querySnapshot, error) in
-            self.messages = []
-            if let e = error {
-                print(e.localizedDescription)
-            } else {
-                if let snapshotDocuments = querySnapshot?.documents {
-                    for doc in snapshotDocuments {
-                        let data = doc.data()
-                        if let messageSender = data[K.chat.FStore.senderField] as? String, let messageText = data[K.chat.FStore.textField] as? String, let messageDate = data[K.chat.FStore.dateField] {
-                            let newMessage = Message(sender: messageSender, body: messageText, date: messageDate as! NSNumber)
+                self.messages = []
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    
+                    if let snapshotDocuments = querySnapshot?.documents {
+                        
+                        print(snapshotDocuments.count)
+                        if snapshotDocuments.count == 0 {
+                            let newMessage =  Message(sender: "Debug", body: "This chat is currently empty...\nWhy not be the first to post", date: 0)
                             self.messages.append(newMessage)
-                            
                             
                             DispatchQueue.main.async {
                                 self.tableView.reloadData()
                                 let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
-                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+                                self.tableView.scrollToRow(at: indexPath, at: .top, animated: false)
                             }
                             
+                            
+                            
                         } else {
-                            print("oh no")
+                            for doc in snapshotDocuments {
+                                let data = doc.data()
+                                if let messageSender = data[K.chat.FStore.senderField] as? String, let messageText = data[K.chat.FStore.textField] as? String, let messageDate = data[K.chat.FStore.dateField] {
+                                    let newMessage = Message(sender: messageSender, body: messageText, date: messageDate as! NSNumber)
+                                    self.messages.append(newMessage)
+                                    
+                                    DispatchQueue.main.async {
+                                        self.tableView.reloadData()
+                                        let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                        self.tableView.scrollToRow(at: indexPath, at: .top, animated: false)
+                                    }
+                                    
+                                } else {
+                                    print("oh no")
+                                }
+                            }
                         }
                     }
                 }
             }
-        }
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
         
         if let messageText = messageTextField.text, let messageSender = Auth.auth().currentUser?.email {
             if messageText != "" {
-                db.collection(K.chat.FStore.collectionName).addDocument(data: [
+                db.collection(chosenChat).addDocument(data: [
                     K.chat.FStore.senderField: messageSender,
                     K.chat.FStore.textField: messageText,
                     K.chat.FStore.dateField: Date().timeIntervalSince1970
@@ -106,6 +131,8 @@ class ChatViewController: UIViewController {
     
 }
 
+//MARK: - Table View Data Source
+
 extension ChatViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return messages.count
@@ -140,12 +167,14 @@ extension ChatViewController: UITableViewDataSource {
     }
 }
 
+//MARK: - Table View Delegate
+
 extension ChatViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
+        
         
         let messageDate = messages[indexPath.row].date
-    
+        
         
         let formattedDate = dateFormatter(messageDate)
         let formattedTime = timeFormatter(messageDate)
@@ -165,3 +194,48 @@ extension ChatViewController: UITableViewDelegate {
     }
 }
 
+
+//MARK: - Picker View Data Source
+
+extension ChatViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return busStops.count + 1
+    }
+}
+
+
+
+
+//MARK: - Picker View Delegate
+
+extension ChatViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        busStops.sort()
+        
+        if row == 0 {
+            return "General"
+        } else {
+            return busStops[row-1]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        let chosenRoute: String?
+        
+        if row == 0 {
+            chosenRoute = "messages"
+        } else {
+            chosenRoute = busStops[row-1]
+        }
+        
+        chosenChat = chosenRoute!
+        
+        loadMessages()
+    }
+}
