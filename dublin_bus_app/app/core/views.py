4ad9f22b3238a930eaa3f4a75c064f7a79e14cb0
EJEgan <eugene.egan1@ucdconnect.ie>
2021-07-20 14:47:02 +0100

    commented out broken code in views

--- dublin_bus_app/app/core/views.py
+++ dublin_bus_app/app/core/views.py
@@ -92,10 +92,10 @@ def MapView(request):
 
     return render(request, "map.html")
 
-def show_user_list(request):
-    user_list = CustomUser.objects.order_by('username')
-    output = ', '.join([user.username for user in user_list])
-    return HttpResponse(output)
+# def show_user_list(request):
+#     user_list = CustomUser.objects.order_by('username')
+#     output = ', '.join([user.username for user in user_list])
+#     return HttpResponse(output)
 
 from core.fare_calculation import fare_crawler
 def FareCalculation(request):
