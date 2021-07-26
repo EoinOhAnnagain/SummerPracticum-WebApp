523dbe654f557deb57cc57db009b425184ef750e
Eoinoha <eoin.ohannagain@ucdconnect.ie>
2021-07-26 02:20:16 +0100

    Add view controller for, inc segue to, CodeBreaker Rules

---
+++ iOS/DBA/DBA/Controller/CodeBreakerRulesViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CodeBreakerRulesViewController.swift
+//  DBA
+//
+//  Created by Eoin Ó'hAnnagáin on 26/07/2021.
+//
+
+import UIKit
+
+class CodeBreakerRulesViewController: UIViewController {
+
+    
+    @IBOutlet var tap: UITapGestureRecognizer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func tapper(_ sender: Any) {
+        print("tapped")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
