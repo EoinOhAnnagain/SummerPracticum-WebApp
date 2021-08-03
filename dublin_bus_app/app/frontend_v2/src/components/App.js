6fd1a6328cd3d3d79539a5c1739cb56f36910152
Hank <mhd0416@gmail.com>
2021-08-03 10:53:33 +0100

    update App.js to function componnent

--- dublin_bus_app/app/frontend_v2/src/components/App.js
+++ dublin_bus_app/app/frontend_v2/src/components/App.js
@@ -1,21 +1,17 @@
 import React, { Component} from "react";
+import { useState, useEffect } from 'react';
 import { Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router} from 'react-router-dom'
 import Login from "./login";
 import Signup from "./signup";
 import Hello from "./hello";
 import {axiosInstance} from "../axiosApi";
 
-class App extends Component {
-    constructor() {
-        super();
-        this.handleLogout = this.handleLogout.bind(this);
-        this.state = {
-            logout: false,
-            username: ""
-        };
-    }
+function App(){
+    const [logout, setLogout] = useState(false);
+    const [username, setUsername] = useState("");
 
-    async handleLogout() {
+    const handleLogout = async () => {
         try {
             const response = await axiosInstance.post('/blacklist/', {
                 "refresh_token": localStorage.getItem("refresh_token")
@@ -23,45 +19,47 @@ class App extends Component {
             localStorage.removeItem('access_token');
             localStorage.removeItem('refresh_token');
             axiosInstance.defaults.headers['Authorization'] = null;
-            this.setState({logout:true});
+            setLogout(true);
             return response;
         }
         catch (e) {
             console.log(e);
         }
-    };
-
-    render() {
-        if (this.state.logout){
-            alert("you are already log out!");
-            this.setState({username: localStorage.getItem('username')});
-            localStorage.removeItem('username');
-            this.setState({logout:false});
-            // redirect to login page.
-        }
-        
-        return (
-            <div className="site">
-                <nav>
-                    <Link className={"nav-link"} to={"/"}>Home</Link>
-                    <Link className={"nav-link"} to={"/login/"}>Login</Link>
-                    <Link className={"nav-link"} to={"/signup/"}>Signup</Link>
-                    <Link className={"nav-link"} to={"/hello/"}>Hello</Link>
-                    {/* if login show logout button */}
-                    <button onClick={this.handleLogout}>Logout</button>
-                </nav>
-                {/* <Welcome name = {this.state.username}/>*/}
-                <main>
-                    <Switch>
-                        <Route exact path={"/login/"} component={Login}/>
-                        <Route exact path={"/signup/"} component={Signup}/>
-                        <Route exact path={"/hello/"} component={Hello}/>
-                        <Route path={"/"} render={() => <div>Home again</div>}/>
-                    </Switch>
-                </main>
-            </div>
-        );
     }
+
+    if (logout){
+        alert("you are already log out!");
+        setUsername(localStorage.getItem('username'));
+        localStorage.removeItem('username');
+        setLogout(false);
+    }
+
+    return (
+        <Router>
+        <div className="container">
+        <div className="site">
+            <nav>
+                <Link className={"nav-link"} to={"/"}>Home</Link>
+                <Link className={"nav-link"} to={"/login/"}>Login</Link>
+                <Link className={"nav-link"} to={"/signup/"}>Signup</Link>
+                <Link className={"nav-link"} to={"/hello/"}>Hello</Link>
+                {/* <Link className={"nav-link"} to={"/map/"}>Map</Link> */}
+                <button onClick={handleLogout}>Logout</button>
+            </nav>
+            
+            <main>
+                <Switch>
+                    <Route exact path={"/login/"} component={Login}/>
+                    <Route exact path={"/signup/"} component={Signup}/>
+                    <Route exact path={"/hello/"} component={Hello}/>
+                    <Route path={"/"} render={() => <div>Home again</div>}/>
+                    {/* <Route exact path='/map/' render={(props) => (<><MainMaps stopData={stopData}/><Navbar stopData={stopData}/></>)}/> */}
+                </Switch>
+            </main>
+        </div>
+        </div>
+        </Router>
+    );
 }
 
 export default App;
