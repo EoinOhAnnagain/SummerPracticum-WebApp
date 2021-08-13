0ac276fae29a28e32fb1b9e0c08c50c5d8e97c91
Hank <mhd0416@gmail.com>
2021-08-13 12:02:49 +0100

    fix weather page bug: location API and weather API async issue

--- dublin_bus_app/app/frontend_v2/src/components/Weather.js
+++ dublin_bus_app/app/frontend_v2/src/components/Weather.js
@@ -2,27 +2,40 @@ import React, { useEffect, useState } from "react";
 import CardExampleCard from "./Weather_card";
 
 
+
 const Weather = ()=>{
     const [lat, setLat] = useState([]);
     const [long, setLong] = useState([]);
     const [data, setData] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            navigator.geolocation.getCurrentPosition(function(position) {
-            setLat(position.coords.latitude);
-            setLong(position.coords.longitude);
-            });
+    const getPosition = () => {
+        return new Promise(function (resolve, reject) {
+          navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+      };
+    
+    const getWeather = async (latitude, longitude) => {
+        setLat(latitude);
+        setLong(longitude);
+        const api_call = await
+        fetch(`${process.env.REACT_APP_OW_API_URL}/weather/?lat=${latitude}&lon=${longitude}&units=metric&APPID=${process.env.REACT_APP_OW_API_KEY}`);
+        const data = await api_call.json();
+        setData(data)
+        console.log('data is: ', data);  
+      };
 
-            await fetch(`${process.env.REACT_APP_OW_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_OW_API_KEY}`)
-            .then(res => res.json())
-            .then(result => {
-            setData(result)
-            console.log(result);
-            });
+    useEffect(() => {
+        const fetchData = () => {
+            getPosition()
+            .then((position) => {
+               getWeather(position.coords.latitude,     
+               position.coords.longitude)
+             })
+             .catch((err) => console.log(err.message));
         }
+
         fetchData();
-      }, [lat, long]);
+      }, []);
 
     return (
         <div className="Weather">
