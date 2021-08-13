80f8f435a2a46c7022135d9aa87e59f1362d3c74
Hank <mhd0416@gmail.com>
2021-08-13 15:35:24 +0100

    using city ID to fetch weather info

--- dublin_bus_app/app/frontend_v2/src/components/Weather.js
+++ dublin_bus_app/app/frontend_v2/src/components/Weather.js
@@ -7,6 +7,15 @@ const Weather = ()=>{
     const [lat, setLat] = useState([]);
     const [long, setLong] = useState([]);
     const [data, setData] = useState([]);
+    
+
+    // const getPosition_g = () => {
+    //     return fetch("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDMShWhoPlx171FYgvYn_nOroBgsf28oCk")
+    //           .then((response) => response.json())
+    //           .then((data) => console.log(data)).catch(function(error) {
+    //             console.log('Looks like there was a problem: \n', error);
+    //           });
+    //     }
 
     const getPosition = () => {
         return new Promise(function (resolve, reject) {
@@ -14,11 +23,12 @@ const Weather = ()=>{
         });
       };
     
-    const getWeather = async (latitude, longitude) => {
-        setLat(latitude);
-        setLong(longitude);
+    const getWeather = async () => {
+        // setLat(latitude);
+        // setLong(longitude);
         const api_call = await
-        fetch(`${process.env.REACT_APP_OW_API_URL}/weather/?lat=${latitude}&lon=${longitude}&units=metric&APPID=${process.env.REACT_APP_OW_API_KEY}`);
+        // fetch(`${process.env.REACT_APP_OW_API_URL}/weather/?lat=${latitude}&lon=${longitude}&units=metric&APPID=${process.env.REACT_APP_OW_API_KEY}`);
+        fetch(`${process.env.REACT_APP_OW_API_URL}/weather/?id=7778677&units=metric&APPID=${process.env.REACT_APP_OW_API_KEY}`);
         const data = await api_call.json();
         setData(data)
         console.log('data is: ', data);  
@@ -26,12 +36,13 @@ const Weather = ()=>{
 
     useEffect(() => {
         const fetchData = () => {
-            getPosition()
-            .then((position) => {
-               getWeather(position.coords.latitude,     
-               position.coords.longitude)
-             })
-             .catch((err) => console.log(err.message));
+            // getPosition()
+            // .then((position) => {
+            //    getWeather(position.coords.latitude,     
+            //    position.coords.longitude)
+            //  })
+            //  .catch((err) => console.log(err.message));
+            getWeather().catch((err) => console.log(err.message));
         }
 
         fetchData();
